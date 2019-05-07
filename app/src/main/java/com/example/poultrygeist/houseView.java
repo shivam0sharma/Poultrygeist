f8712e1193415f667f5e795d19115191108d9e40
rahul <rahul199722@gmail.com>
2019-05-07 12:05:43 -0400

    House view added

---
+++ app/src/main/java/com/example/poultrygeist/houseView.java
@@ -0,0 +1,13 @@
+package com.example.poultrygeist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class houseView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_house_view);
+    }
+}
