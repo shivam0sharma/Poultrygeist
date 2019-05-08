8741b86c1ec903bb2d3fbfb3e95cdf24ba958600
rahul <rahul199722@gmail.com>
2019-05-08 13:29:02 -0400

    Added Partially implemented HouseView

--- app/src/main/java/com/example/poultrygeist/houseView.java
+++ app/src/main/java/com/example/poultrygeist/houseView.java
@@ -3,11 +3,118 @@ package com.example.poultrygeist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class houseView extends AppCompatActivity {
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 
+public class houseView extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener{
+    private RelativeLayout parentLayout ;
+    private static final String TAG = "MainActivity";
+    private View pointSelected = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_house_view);
+
+        parentLayout = (RelativeLayout) findViewById(R.id.Map);
+        createButton(10,30, 1);
+//        Button b = new Button(this);
+//        b.setWidth(10);
+//        b.setHeight(10);
+//        b.setBackgroundColor((int)R.color.colorPrimary);
+//        b.setId((int) 1);
+//        RelativeLayout.LayoutParams rel_btn = new RelativeLayout.LayoutParams(
+//                50, 50);
+//        rel_btn.leftMargin = 120;
+//        rel_btn.topMargin = 20;
+//
+//
+//        b.setLayoutParams(rel_btn);
+//
+//
+//        b.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d(TAG, "onClick: Button clicked");
+//            }
+//        });
+//
+//        parentLayout.addView(b);
+    }
+    void createButton(int x, int y, int id) {
+        Button b = new Button(this);
+        b.setWidth(10);
+        b.setHeight(10);
+        b.setBackgroundColor((int)R.color.colorPrimary);
+        b.setId((int) 1);
+        RelativeLayout.LayoutParams rel_btn = new RelativeLayout.LayoutParams(
+                50, 50);
+        rel_btn.leftMargin = x;
+        rel_btn.topMargin = y;
+        Log.d(TAG, "createButton: " + b.getId());
+
+
+        b.setLayoutParams(rel_btn);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: Button clicked");
+                onPointClick(v);
+            }
+        });
+
+        Log.d(TAG, "onCreate: I am here!" + b.getWidth());
+        parentLayout.addView(b);
+    }
+
+    void onPointClick(View v) {
+        pointSelected = v;
+        PopupMenu popup = new PopupMenu(this, v);
+        MenuInflater inflater = popup.getMenuInflater();
+        popup.setOnMenuItemClickListener(houseView.this);
+        inflater.inflate(R.menu.menu_chicken, popup.getMenu());
+        popup.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+//        pointSelected.addExtraDataToAccessibilityNodeInfo();
+//        pointSelected.performAccessibilityAction()
+        switch (item.getItemId()) {
+            case R.id.Remove:
+                Log.d(TAG, "onMenuItemClick: "+ pointSelected.getId());
+                ((ViewGroup)(pointSelected.getParent())).removeView(pointSelected);
+                pointSelected = null;
+                Log.d(TAG, "onMenuItemClick: Chicken Removed");
+                return true;
+
+            case R.id.FalseAlarm:
+                Log.d(TAG, "onMenuItemClick: "+ pointSelected.getId());
+                ((ViewGroup)(pointSelected.getParent())).removeView(pointSelected);
+                pointSelected = null;
+                Log.d(TAG, "onMenuItemClick: False Alarm");
+                return true;
+
+            case R.id.WrongPosition:
+                Log.d(TAG, "onMenuItemClick: "+ pointSelected.getId());
+                ((ViewGroup)(pointSelected.getParent())).removeView(pointSelected);
+                pointSelected = null;
+                Log.d(TAG, "onMenuItemClick: Chicken Removed");
+                return true;
+
+            case R.id.Cancel:
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
